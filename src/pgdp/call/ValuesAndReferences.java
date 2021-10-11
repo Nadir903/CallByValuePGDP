b8d5390f9e26bb6c02e03a6f57641333c1a2f3b5
Robin Marchart <robin.marchart@tum.de>
2021-10-11 17:48:41 +0200

    push code example

---
+++ src/pgdp/call/ValuesAndReferences.java
@@ -0,0 +1,40 @@
+package pgdp.call;
+
+public class ValuesAndReferences {
+
+    public static void main(String[] args) {
+        ref();
+    }
+
+    public static void ref() {
+        int n = 1;
+        A a1 = new A(1);
+        A a2 = new A(1);
+
+        setNumber(n);
+        setAttribute(a1);
+        setObject(a2);
+
+        System.out.println(n);
+        System.out.println(a1.n);
+        System.out.println(a2.n);
+    }
+
+    static void setNumber(int n) {
+        n = 2;
+    }
+    static void setAttribute(A a) {
+        a.n = 2;
+    }
+    static void setObject(A a) {
+        a = new A(2);
+    }
+}
+
+class A {
+    int n;
+
+    public A(int n) {
+        this.n = n;
+    }
+}
